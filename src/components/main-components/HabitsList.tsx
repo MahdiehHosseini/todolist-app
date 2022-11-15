6bef5198fd870a1727b4512d859cfe96aba3f8ea
Mahdieh Hosseini <mahdieh1384.sh@gmail.com>
2022-11-15 18:43:49 +0330

    update

--- src/components/main-components/HabitsList.tsx
+++ src/components/main-components/HabitsList.tsx
@@ -14,13 +14,13 @@ const HabitsList: FC<HabitsListComponentInterface> = ({habitData}) => {
 		}
 	}
 	sliceIntoChunks(habitData)
-	const boxes = splitTwo.map(twoTasks => twoTasks.length === 2 
+	const boxes = splitTwo.map(twoHabits => twoHabits.length === 2 
 		? <div className='md:flex md:justify-around'>
-			<SingleHabitBox key={twoTasks[0].id} habitData={twoTasks[0]} />
-			<SingleHabitBox key={twoTasks[1].id} habitData={twoTasks[1]} />
+			<SingleHabitBox key={twoHabits[0].id} habitData={twoHabits[0]} />
+			<SingleHabitBox key={twoHabits[1].id} habitData={twoHabits[1]} />
 		</div> 
 		: <div className='md:w-full md:flex md:justify-center'>
-			<SingleHabitBox key={twoTasks[0].id} habitData={twoTasks[0]} />
+			<SingleHabitBox key={twoHabits[0].id} habitData={twoHabits[0]} />
 		</div> )
 	return (
 		<section className='mx-6 relative mb-20'>
